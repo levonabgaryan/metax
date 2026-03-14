aab121a2ccd9f4c190e1dbd463879181e677357e
Levon Abgaryan <levonabgaryan177@gmail.com>
2026-03-14 19:27:02 +0400

    Configure non blocking IO logs

--- logger/logger.py
+++ logger/logger.py
@@ -1,5 +1,9 @@
+import atexit
 import logging
+import queue
 import sys
+from logging.handlers import QueueListener, QueueHandler
+
 from config_ import discount_service_configs
 
 if discount_service_configs.debug:
@@ -23,10 +27,7 @@ file_handler = logging.FileHandler("discount_service.log", mode="a", encoding="u
 file_handler.setFormatter(formatter)
 file_handler.setLevel(log_level)
 
-root_logger.addHandler(console_handler)
-root_logger.addHandler(file_handler)
-
-# Packages, libs loggers config
+# --- Side-libraries logging levels configuration ---
 PACKAGE_LOG_LEVEL = logging.DEBUG if discount_service_configs.debug else logging.WARNING
 packages_loggers_names = [
     "django",
@@ -44,3 +45,16 @@ packages_loggers_names = [
 for logger_name in packages_loggers_names:
     logger = logging.getLogger(logger_name)
     logger.setLevel(PACKAGE_LOG_LEVEL)
+
+# --- Async-friendly logging (Non-blocking) ---
+log_queue: queue.Queue[logging.LogRecord] = queue.Queue(-1)
+queue_handler = QueueHandler(queue=log_queue)
+handlers: list[logging.Handler] = [console_handler, file_handler]
+listener = QueueListener(log_queue, *handlers, respect_handler_level=True)
+root_logger.addHandler(queue_handler)
+
+# --- Lifecycle management ---
+# Start the background listener thread and ensure it drains the queue on exit
+listener.start()
+
+atexit.register(listener.stop)
