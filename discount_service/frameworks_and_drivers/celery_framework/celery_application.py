24e8ff1e946ad9960b7cee33952439b0e2ce1d17
Levon Abgaryan <levonabgaryan177@gmail.com>
2026-03-15 18:00:23 +0400

    Add celery logger config

--- discount_service/frameworks_and_drivers/celery_framework/celery_application.py
+++ discount_service/frameworks_and_drivers/celery_framework/celery_application.py
@@ -1,8 +1,13 @@
+import logging
+from typing import Any
+
 from celery import Celery
 from celery.schedules import crontab
+from celery.signals import after_setup_logger
 
 from config_ import discount_service_configs
 from discount_service.frameworks_and_drivers.di import get_service_container
+from logger.logger import init_logger
 
 celery_app = Celery(
     main="discount_service",
@@ -25,3 +30,12 @@ celery_app.conf.beat_schedule = {
         "options": {"queue": "default"},
     },
 }
+
+
+@after_setup_logger.connect
+def setup_celery_logger(logger: logging.Logger, *args: Any, **kwargs: Any) -> None:
+    """
+    This signal ensures that when Celery starts its worker,
+    it initializes project's custom non-blocking logger.
+    """
+    init_logger()
